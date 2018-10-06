3b20934458db7d8f1713ca382cd534c33c3a95d5
Marek <marekz@gmail.com>
2018-10-06 16:11:26 -0400

    Update type definitions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,46 @@
-declare module 'liquidjs';
-declare module 'fs-extra';
-declare module 'pdf-parse';
+import * as puppeteer from 'puppeteer';
 
-interface IServerOptions {
+export interface IServerOptions {
   templatesDirectory: string;
   assetsDirectory: string;
+}
+
+interface IConstructorOptions {
+  browser: puppeteer.Browser;
+  port: string | number;
+  server: IStoppable;
+}
+
+interface ITemplateValues {
+  [key: string]: any;
+}
+
+interface IStoppable {
+  stop(): void;
+}
+
+interface IPdfOptions {
+  width: string;
+  height: string;
+  margin: {
+    top: string;
+    bottom: string;
+    left: string;
+    right: string;
+  };
+}
+
+interface IPrinterOptions {
+  waitUntilEvent: 'networkidle0' | 'networkidle2' | 'load';
+}
+
+interface IPistonPrinter {
+  printTemplate(
+    templateName: string,
+    values?: ITemplateValues,
+    options?: Partial<IPdfOptions>,
+    printerOptions?: Partial<IPrinterOptions>
+  ): Promise<{ pdf: Buffer }>;
+
+  close(): void;
 }
